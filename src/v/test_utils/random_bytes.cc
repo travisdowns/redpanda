ab44525aca67e7df33b64261ce36a87c01fb2b09
Travis Downs <travis.downs@redpanda.com>
2025-08-05 10:21:01 -0400

    random: misc cleanups
    Various cleanups which are a precursor to the subsequent changes, e.g.,
    missing headers.

--- src/v/test_utils/random_bytes.cc
+++ src/v/test_utils/random_bytes.cc
@@ -8,6 +8,7 @@
  * the Business Source License, use of this software will be governed
  * by the Apache License, Version 2.0
  */
+#include "bytes/bytes.h"
 #include "bytes/details/io_fragment.h"
 #include "bytes/iobuf.h"
 #include "random/generators.h"
