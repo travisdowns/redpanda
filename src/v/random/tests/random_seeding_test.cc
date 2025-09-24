846ba53e232e9b25999305fbf33b3f5417257440
Travis Downs <travis.downs@redpanda.com>
2025-09-24 18:08:23 -0300

    reword: random seeding tests

--- src/v/random/tests/random_seeding_test.cc
+++ src/v/random/tests/random_seeding_test.cc
@@ -7,19 +7,47 @@
 // the Business Source License, use of this software will be governed
 // by the Apache License, Version 2.0
 
+#if IS_SEASTAR_TEST == 0 && defined(IS_BTEST)
 #define BOOST_TEST_MODULE random_seeding
+#endif
 
-#include "random/fast_prng.h"
 #include "random/generators.h"
+#include "test_utils/test_macros.h"
 
+// This test gets compiled into three variants:
+// non-seastar boost
+// seastar boost
+// gtest
+// Hence the macro boilerplate below.
+
+#ifdef IS_GTEST
+
+#include <gtest/gtest.h>
+#define MAKE_TEST_CASE(name) TEST(random_seeding, name)
+
+#elif IS_BTEST
+
+#if IS_SEASTAR_TEST == 0
 #include <boost/test/unit_test.hpp>
+#define MAKE_TEST_CASE BOOST_AUTO_TEST_CASE
+#elif IS_SEASTAR_TEST == 1
+#include <seastar/testing/thread_test_case.hh>
+#define MAKE_TEST_CASE SEASTAR_THREAD_TEST_CASE
+#else
+#error "IS_SEASTAR_TEST must be 0 or 1"
+#endif
 
-#include <set>
+#else
+
+// the cc_test macros define these
+#error one of IS_GTEST or IS_BTEST must be defined
+
+#endif
 
 using random_generators::get_int;
-using random_generators::random_state;
+using random_generators::rng;
 
-// Any object of random_state encapsulates RNG state. I.e., two random_state
+// Any object of rng encapsulates RNG state. I.e., two rng
 // objects with the same state will generate the same sequence of random
 // numbers.
 //
@@ -27,16 +55,34 @@ using random_generators::random_state;
 // existing callers of the free-function interface, i.e.,
 // `random_generators::get_int()` becomes
 // `random_generators::global().get_int()`, but new use should consider creating
-// and maintaining their own random_state object.
+// and maintaining their own rng object.
 
-constexpr int fixed_seed = 0xBADF00D;
+struct random_state_accessor {};
 
-BOOST_AUTO_TEST_CASE(test_expected_values) {
-    random_state rng{fixed_seed};
-    BOOST_CHECK_EQUAL(rng.get_int<int>(), 880186458);
+// the first and second values expected from `get_int` with the default seed
+constexpr int get_int_0 = 1950485216;
+constexpr int get_int_1 = 472535779;
+
+void check_two_random_numbers() {
+    RPTEST_EXPECT_EQ(get_int<int>(), get_int_0);
+    RPTEST_EXPECT_EQ(get_int<int>(), get_int_1);
 }
 
-BOOST_AUTO_TEST_CASE(test_expected_values2) {
-    random_state rng{fixed_seed};
-    BOOST_CHECK_EQUAL(rng.get_int<int>(), 880186458);
+MAKE_TEST_CASE(test_expected_values) { check_two_random_numbers(); }
+
+MAKE_TEST_CASE(test_expected_values2) {
+    // same as the above test, but since we run both tests in the same process
+    // (usually) it tests that the global rng state is being reset between
+    // tests.
+    check_two_random_numbers();
+}
+
+MAKE_TEST_CASE(test_local_reseeding) {
+    rng local_rng;
+    RPTEST_EXPECT_EQ(local_rng.get_int<int>(), get_int_0);
+    RPTEST_EXPECT_EQ(local_rng.get_int<int>(), get_int_1);
+
+    local_rng = rng{};
+    RPTEST_EXPECT_EQ(local_rng.get_int<int>(), get_int_0);
+    RPTEST_EXPECT_EQ(local_rng.get_int<int>(), get_int_1);
 }
