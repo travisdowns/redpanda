7d5349e35f2a36f82873bcb674d7e63fc16ef0b8
Travis Downs <travis.downs@redpanda.com>
2025-09-24 17:28:42 -0300

    squash: also replace internal::seed with global().intitial_seed()

--- src/v/cloud_storage/tests/remote_partition_fuzz_test.cc
+++ src/v/cloud_storage/tests/remote_partition_fuzz_test.cc
@@ -17,12 +17,11 @@
 #include "cloud_storage/tests/util.h"
 #include "cloud_storage/types.h"
 #include "model/record_batch_types.h"
+#include "random/generators.h"
 
 #include <seastar/core/lowres_clock.hh>
 
 #include <fmt/chrono.h>
-
-#include <random>
 
 using namespace cloud_storage;
 
@@ -209,7 +208,7 @@ FIXTURE_TEST(
     vlog(
       test_log.info,
       "Seed used for read workload: {}",
-      random_generators::internal::seed);
+      random_generators::global().initial_seed());
 
     constexpr int num_segments = 1000;
     const auto [batch_types, num_data_batches] = generate_segment_layout(
@@ -237,7 +236,7 @@ FIXTURE_TEST(
     vlog(
       test_log.info,
       "Seed used for read workload: {}",
-      random_generators::internal::seed);
+      random_generators::global().initial_seed());
 
     constexpr int num_segments = 1000;
     const auto [batch_types, num_data_batches] = generate_segment_layout(
@@ -267,7 +266,7 @@ FIXTURE_TEST(
     vlog(
       test_log.info,
       "Seed used for read workload: {}",
-      random_generators::internal::seed);
+      random_generators::global().initial_seed());
 
     constexpr int num_segments = 1000;
     const auto [batch_types, num_data_batches] = generate_segment_layout(
@@ -310,7 +309,7 @@ FIXTURE_TEST(
     vlog(
       test_log.info,
       "Seed used for read workload: {}",
-      random_generators::internal::seed);
+      random_generators::global().initial_seed());
 
     constexpr int num_segments = 1000;
     const auto [segment_layout, num_data_batches] = generate_segment_layout(
@@ -351,7 +350,7 @@ FIXTURE_TEST(
     vlog(
       test_log.info,
       "Seed used for read workload: {}",
-      random_generators::internal::seed);
+      random_generators::global().initial_seed());
 
     constexpr int num_segments = 1000;
     const auto [segment_layout, num_data_batches] = generate_segment_layout(
@@ -394,7 +393,7 @@ FIXTURE_TEST(
     vlog(
       test_log.info,
       "Seed used for read workload: {}",
-      random_generators::internal::seed);
+      random_generators::global().initial_seed());
 
     constexpr int num_segments = 1000;
     const auto [batch_types, num_data_batches] = generate_segment_layout(
